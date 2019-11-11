8bd8a4f441c410a9f9ce2f7e3a036336f27b2ee5
lucas2610 <38497046+lucas2610@users.noreply.github.com>
2019-11-11 13:17:43 -0300

    Ex2 adicionado

---
+++ ExemploCrud/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ExemploCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbNovo = new System.Windows.Forms.ToolStripButton();
+            this.tsbCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalvar = new System.Windows.Forms.ToolStripButton();
+            this.tsbAlterar = new System.Windows.Forms.ToolStripButton();
+            this.tsbExcluir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtBoxBuscar = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbPesquisar = new System.Windows.Forms.ToolStripButton();
+            this.Id = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNovo,
+            this.tsbCancelar,
+            this.tsbSalvar,
+            this.tsbAlterar,
+            this.tsbExcluir,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.txtBoxBuscar,
+            this.tsbPesquisar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbNovo
+            // 
+            this.tsbNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNovo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNovo.Image")));
+            this.tsbNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNovo.Name = "tsbNovo";
+            this.tsbNovo.Size = new System.Drawing.Size(23, 22);
+            this.tsbNovo.Text = "Novo";
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsbCancelar.Image")));
+            this.tsbCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(23, 22);
+            this.tsbCancelar.Text = "Cancelar";
+            // 
+            // tsbSalvar
+            // 
+            this.tsbSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSalvar.Image = ((System.Drawing.Image)(resources.GetObject("tsbSalvar.Image")));
+            this.tsbSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalvar.Name = "tsbSalvar";
+            this.tsbSalvar.Size = new System.Drawing.Size(23, 22);
+            this.tsbSalvar.Text = "Salvar";
+            this.tsbSalvar.Click += new System.EventHandler(this.tsbSalvar_Click);
+            // 
+            // tsbAlterar
+            // 
+            this.tsbAlterar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbAlterar.Image = ((System.Drawing.Image)(resources.GetObject("tsbAlterar.Image")));
+            this.tsbAlterar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAlterar.Name = "tsbAlterar";
+            this.tsbAlterar.Size = new System.Drawing.Size(23, 22);
+            this.tsbAlterar.Text = "Alterar";
+            this.tsbAlterar.Click += new System.EventHandler(this.tsbAlterar_Click);
+            // 
+            // tsbExcluir
+            // 
+            this.tsbExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbExcluir.Image = ((System.Drawing.Image)(resources.GetObject("tsbExcluir.Image")));
+            this.tsbExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExcluir.Name = "tsbExcluir";
+            this.tsbExcluir.Size = new System.Drawing.Size(23, 22);
+            this.tsbExcluir.Text = "Excluir";
+            this.tsbExcluir.Click += new System.EventHandler(this.tsbExcluir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(76, 22);
+            this.toolStripLabel1.Text = "Buscar por Id";
+            // 
+            // txtBoxBuscar
+            // 
+            this.txtBoxBuscar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtBoxBuscar.Name = "txtBoxBuscar";
+            this.txtBoxBuscar.Size = new System.Drawing.Size(100, 25);
+            // 
+            // tsbPesquisar
+            // 
+            this.tsbPesquisar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbPesquisar.Image = ((System.Drawing.Image)(resources.GetObject("tsbPesquisar.Image")));
+            this.tsbPesquisar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPesquisar.Name = "tsbPesquisar";
+            this.tsbPesquisar.Size = new System.Drawing.Size(23, 22);
+            this.tsbPesquisar.Text = "Pesquisar";
+            this.tsbPesquisar.Click += new System.EventHandler(this.tsbPesquisar_Click);
+            // 
+            // Id
+            // 
+            this.Id.AutoSize = true;
+            this.Id.Location = new System.Drawing.Point(12, 47);
+            this.Id.Name = "Id";
+            this.Id.Size = new System.Drawing.Size(16, 13);
+            this.Id.TabIndex = 1;
+            this.Id.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(34, 40);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(196, 41);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Endereco";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(74, 73);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(222, 20);
+            this.txtEndereco.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.Id);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbNovo;
+        private System.Windows.Forms.ToolStripButton tsbCancelar;
+        private System.Windows.Forms.ToolStripButton tsbSalvar;
+        private System.Windows.Forms.ToolStripButton tsbAlterar;
+        private System.Windows.Forms.ToolStripButton tsbExcluir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtBoxBuscar;
+        private System.Windows.Forms.ToolStripButton tsbPesquisar;
+        private System.Windows.Forms.Label Id;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEndereco;
+    }
+}
+
---
+++ ExemploCrud/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ExemploCrud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlCon = null; 
+        private String strCon = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Documents\BancoCrud.mdf;Integrated Security=True;Connect Timeout=30";
+        private String strSql = string.Empty;
+        private int teste;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbSalvar_Click(object sender, EventArgs e)
+        {
+            strSql = "insert into funcionarios(Id, Nome, Endereco) values(@Id, @Nome, @Endereco)";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql,sqlCon);
+          
+            comando.Parameters.Add("@Id",SqlDbType.Int).Value=txtId.Text;
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value=txtNome.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value=txtEndereco.Text;
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Sucesso");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro!"+ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+
+        private void tsbPesquisar_Click(object sender, EventArgs e)
+        {
+            strSql = "select *from funcionarios where Id=@Id";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+            MessageBox.Show(txtBoxBuscar.Text);
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value = txtBoxBuscar.Text;
+
+            try
+            {
+                if (txtBoxBuscar.Text == string.Empty)
+                {
+                    throw new Exception("Você precisa digitar um id!");
+                }
+
+                sqlCon.Open();
+                SqlDataReader dr = comando.ExecuteReader();
+
+               
+               // MessageBox.Show(Convert.ToString(dr["Id"]));
+
+                if (dr.HasRows == false)
+                {
+                    throw new Exception("É vazio!");
+                }
+               
+              
+               while(dr.Read())
+                {
+                    txtId.Text = Convert.ToString(dr["Id"]);
+                    txtNome.Text = Convert.ToString(dr["Nome"]);
+                    txtEndereco.Text = Convert.ToString(dr["Endereco"]);
+                }
+               
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro!" + ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void tsbExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente Excluir?","Cuidado",MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2)==DialogResult.No)
+            {
+                MessageBox.Show("Operação Cancelada!");
+            }
+            else
+            {
+                strSql = "delete from funcionarios where Id=@Id";
+                sqlCon = new SqlConnection(strCon);
+                SqlCommand comando = new SqlCommand(strSql, sqlCon);
+                comando.Parameters.Add("@Id", SqlDbType.Int).Value = txtBoxBuscar.Text;
+                try
+                {
+                    sqlCon.Open();
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Funcionario Excluido!");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        private void tsbAlterar_Click(object sender, EventArgs e)
+        {
+            strSql = "update funcionarios set  Nome=@Nome, Endereco=@Endereco where Id=@Id";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value =txtBoxBuscar.Text;
+
+           
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = txtEndereco.Text;
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Alteração feita");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
